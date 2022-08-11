c700832653154e9e67462e6a0b8b41d5025492fe
Xiagir <513535b@gmail.com>
2022-08-11 15:04:50 -0400

    disabled random button to fix a bug

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -55,7 +55,7 @@ const Header = () => {
               </Link>
             </li>
             <li>
-              <Link href={`/anime/${random}`}>
+              <Link>
                 <a
                   className="block py-2 pr-4 pl-3  md:p-0 text-gray-400 md:hover:text-white hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700"
                   aria-current="page"
