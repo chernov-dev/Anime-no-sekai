fb784de7e6af65ba3c3000e8942340f808eebe3e
Xiagir <513535b@gmail.com>
2022-08-11 22:46:21 -0400

    fixed animePreviewCard redirect / Redesigned mobile anime preview card

--- components/AnimeComponents/AnimeCard/AnimeComponent.jsx
+++ components/AnimeComponents/AnimeCard/AnimeComponent.jsx
@@ -1,63 +1,69 @@
 import Image from "next/image";
-import React from "react";
 import Router from "next/router";
+import { ReadMore } from "../../utils/ReadMore";
 
-import { BsShareFill } from "react-icons/bs";
-import { BsFillCalendarCheckFill } from "react-icons/bs";
-import { BsHeartFill } from "react-icons/bs";
+import {
+  BsFillCalendarCheckFill,
+  BsHeartFill,
+  BsShareFill,
+} from "react-icons/bs";
+import { useRef } from "react";
 
 const AnimeComponent = (props) => {
   const { anime } = props;
+  const cardRef = useRef(null);
+
   return (
     <div
-      className="anime-preview-card animate__animated animate__fadeIn"
+      className="anime-preview-card"
+      ref={cardRef}
       style={{
         backgroundImage: `url(https://static.openni.ru/${anime.screenImage[0]})`,
       }}
+      onClick={(e) => {
+        Router.push(`/anime/${anime.id}`);
+      }}
     >
-      <div
-        className="anime-preview-card__overlay"
-        onClick={() => {
-          Router.push(`/anime/${anime.id}`);
-        }}
-      ></div>
-      <div className="anime-preview-card__content">
+      {/* <div className="anime-preview-card__overlay"></div> */}
+      <div className="anime-preview-card__wrapper">
         <div className="anime-preview-card__header">
           <div className="anime-preview-card__img">
             <Image
               src={anime.urlImagePreview}
               className=""
               alt="preview"
-              width="80px"
-              height="120px"
+              width="150px"
+              height="225px"
               layout="fixed"
             />
           </div>
-          <h4 className="anime-preview-card__title">
-            {anime.title.split("/")[0]}
-          </h4>
-          <h4 className="anime-preview-card__info">
-            {anime.year}, {anime.director}
-          </h4>
-          <div className="anime-preview-card__type">
-            <p>{anime.type}</p>
-            {anime.genre}
+          <div className="anime-preview-card__header-content">
+            <h4 className="anime-preview-card__title">
+              {anime.title.split("/")[0]}
+            </h4>
+            <h4 className="anime-preview-card__info">
+              {anime.year}, {anime.director}
+            </h4>
+            <div className="anime-preview-card__type">
+              <p>{anime.type}</p>
+              {anime.genre}
+            </div>
           </div>
         </div>
         <div className="anime-preview-card__body">
           <p className="anime-preview-card__desc short">
-            {anime.description.split("<br />")[0]}
+            {anime.description.slice(0, 150)}...
           </p>
         </div>
         <div className="anime-preview-card__footer">
           <div className="anime-preview-card__share">
-            <span onClick={() => console.log("go")}>
+            <span onClick={(e) => e.stopPropagation()}>
               <BsHeartFill />
             </span>
-            <span onClick={() => console.log("go")}>
+            <span onClick={(e) => e.stopPropagation()}>
               <BsFillCalendarCheckFill />
             </span>
-            <span onClick={() => handleClick()}>
+            <span onClick={(e) => e.stopPropagation()}>
               <BsShareFill />
             </span>
           </div>
